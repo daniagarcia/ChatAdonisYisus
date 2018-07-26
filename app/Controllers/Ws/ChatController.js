300915462534b1a585a8d4393d3ec02d1bcd48c8
usuario-PC\usuario <dania_monserrat@live.com>
2018-07-25 23:24:20 -0500

    nose

--- app/Controllers/Ws/ChatController.js
+++ app/Controllers/Ws/ChatController.js
@@ -4,6 +4,12 @@ class ChatController {
   constructor ({ socket, request }) {
     this.socket = socket
     this.request = request
+
+    socket.on('message', (data) => {
+    })
+    
+    // emit events
+    socket.emit('id', socket.id)
   }
 }
 
--- start/socket.js
+++ start/socket.js
@@ -28,35 +28,35 @@ Ws.channel('chat',({socket})=>{
 
 //se suscriben y se mantiene la coneccion por canal por array o string
 
-Ws.channel('chat', ({ socket }) => {
-  socket.emit("new:id",socket.id);
+// Ws.channel('chat', ({ socket }) => {
+//   socket.emit("new:id",socket.id);
 
-  console.log(socket.topic)
-  Ws.on("connection", (data) => {
-    res.status(200).send("oda");
-  });
+//   console.log(socket.topic)
+//   Ws.on("connection", (data) => {
+//     res.status(200).send("oda");
+//   });
 
 
-  socket.on('Message', (data) => {
-    socket.broadcast('usuario', true)
-  });
+//   socket.on('Message', (data) => {
+//     socket.broadcast('usuario', true)
+//   });
 
-  socket.on('New-message', (data) => {
-    socket.broadcast('usuario', true);
-  });
-});
+//   socket.on('New-message', (data) => {
+//     socket.broadcast('usuario', true);
+//   });
+// });
 
-Ws.channel('chat:*', ({ socket }) => {
-  console.log(socket.topic)
-  for (let i = 0; i < array.length; i++) {
+// Ws.channel('chat:*', ({ socket }) => {
+//   console.log(socket.topic)
+//   for (let i = 0; i < array.length; i++) {
     
-    if (socket.topic="asd") 
-    {
+//     if (socket.topic="asd") 
+//     {
       
-    }
+//     }
     
-  }
-});
+//   }
+// });
 
 
 
